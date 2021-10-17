fd1addc5cf3acef8743635b03781bdb9d01b0518
shahidsid1998 <codewithsid1998@gmail.com>
2021-10-17 14:00:49 +0530

    go file added to repo

---
+++ go.py
@@ -0,0 +1,29 @@
+import fun 
+import time
+print("-----------------Welcome--------------")
+time.sleep(1)
+print("Loading 10%....")
+time.sleep(1)
+print("Loading 30%....")
+time.sleep(1)
+print("Loading 70%....")
+time.sleep(1)
+print("Loading Complete :)")
+a = int(input("Enter The First Number: "))
+b = input("Enter Your Choice: ")
+c = int(input("Enter The Second Number: "))
+
+if (b == '+'):
+ print("Here Is Your Answer: ",fun.add(a , c))
+elif(b == '-'):
+ print("Here Is Your Answer: ",fun.sub(a , c))
+elif(b == '*'):
+ print("Here Is Your Answer: ",fun.mul(a , c))
+elif(b == '/'):
+ print("Here Is Your Answer: ",fun.div(a , c))
+else:
+ print("Please Enter Valid Input")
+time.sleep(2)
+print("---------Basic Calculater By Shahid----------------\n")
+time.sleep(1)
+print("--------Happy ending--------------")
